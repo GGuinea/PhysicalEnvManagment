d9bc0fe4caea8070b7f84b515d99f03541df9408
Gguinea <Gguinea>
2022-04-04 22:15:13 +0200

    Working todo app

--- src/components/TodoSearchDisplay.js
+++ src/components/TodoSearchDisplay.js
@@ -1,52 +1,30 @@
 import React, { useState } from "react";
-import { RiCloseCircleLine } from "react-icons/ri";
-import { TiEdit } from "react-icons/ti";
-import TodoForm from "./TodoForm";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import Dialog from "@material-ui/core/Dialog";
-import Button from "@material-ui/core/Button";
+import Modal from "./Modal";
 
-function TodoSearchDisplay({ todos, completeTodo }) {
-  const [open, setOpen] = React.useState(false);
+function TodoSearchDisplay({ todos }) {
   const [todoItem, setTodoItem] = useState("");
 
   const handleClickOpen = (todo) => {
     setTodoItem(todo);
     console.log(todo);
-    setOpen(true);
+    setIsOpen(true);
   };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return todos.map((todo, index) => (
-    <div
-      className={todo.isComplete ? "todo-row complete" : "todo-row"}
-      key={index}
-    >
+  const [isOpen, setIsOpen] = useState(false);
+const list = todos.map((todo, index) => 
+     <div className="todo-row" key={index}>
       <div key={todo.id} onClick={() => handleClickOpen(todo)}>
         {todo.text}
       </div>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>{todoItem.text}</DialogTitle>
-        <DialogContent>
-            <DialogContent></DialogContent>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Nie
-          </Button>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Tak
-          </Button>
-        </DialogActions>
-      </Dialog>
     </div>
-  ));
+)
+
+return (
+<div>
+    {list}
+      {isOpen && <Modal setOpenModal={setIsOpen} todoItem={todoItem} />}
+</div>
+)
 }
 
 export default TodoSearchDisplay;
